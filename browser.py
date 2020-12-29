495d37332edf05adf18e2fbea839959b9c2b4b54
RozzershotZ <q13rtaylor@wgsf.net>
2020-12-29 20:49:43 +0000

    added cache

--- browser.py
+++ browser.py
@@ -1,6 +1,39 @@
 import requests
 from rich import print
+import os.path
+import json
 
-r = requests.get('https://swapi.dev/api/planets/1/')
-planet = r.json()
+def make_request(path):
+    cachefile = make_cache_file_name(path)
+    if check_file_exists(cachefile):
+        return get_cache_data(cachefile)
+    response = requests.get("https://swapi.dev/api/" + path)
+    json = response.json()
+    file1 = open(cachefile,"w+")
+    file1.write(response.text)
+    file1.close()
+    return json
+
+def get_cache_data(file):
+    with open(file) as json_file:
+        return json.load(json_file)
+
+def setup():
+    if os.path.isdir('cache'):
+        return
+    os.mkdir("cache")
+
+def check_file_exists(file):
+    return os.path.isfile(file)
+
+def make_cache_file_name(path):
+    strippedpath = path.strip("/").replace("/", ".")
+    return "cache/" + strippedpath + ".json"
+
+setup()
+
+planet = make_request('planets/9/')
 print(planet["name"])
+
+ship = make_request("starships/9/")
+print(ship["name"])
